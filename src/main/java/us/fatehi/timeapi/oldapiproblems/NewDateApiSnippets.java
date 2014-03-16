362680c34c020134bd388992907ce59f1c94274b
Sualeh Fatehi <sualeh.fatehi@gmail.com>
2014-03-16 18:53:26 -0400

    New version of PowerPoint

--- src/main/java/us/fatehi/timeapi/oldapiproblems/NewDateApiSnippets.java
+++ src/main/java/us/fatehi/timeapi/oldapiproblems/NewDateApiSnippets.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2014 Sualeh Fatehi
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US.
+ */
 package us.fatehi.timeapi.oldapiproblems;
 
 import java.util.TimeZone;
